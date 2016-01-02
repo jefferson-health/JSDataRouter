9d54633c66078f65cc9488eeccede3ad0fab3d33
David Ramirez 2 <david.x.ramirez@jefferson.edu>
2016-01-01 21:14:46 -0500

    Added an options hash for disabling actions by default. Also returns this instead of this.router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 'use strict';
 
 var express = require('express');
+var param = require('./param');
+
 var findAll = require('./findAll');
 var find = require('./find');
 var create = require('./create');
-var param = require('./param');
+var update = require('./update');
+
+var defaultOptions = {
+  enabledActions:{
+    findAll: true,
+    find: true,
+    create: true,
+    update: true,
+    destroy: true
+  }
+};
 
 class JSDataRouter {
 
-	constructor(resource, router) {
+  constructor(resource, router, options) {
+    options = Object.assign({}, defaultOptions, options);
+
     if(router === undefined) {
       router = express.Router(); 
     }
@@ -17,23 +31,43 @@ class JSDataRouter {
     this.resource = resource;
     this.router.param('resourceId', param.bind(this));
 
-		this.router.get('/',
-			this.beforeFindAll.bind(this),
-			this.findAll.bind(this),
-			this.afterFindAll.bind(this));
+    if(options.enabledActions.findAll) {
+      this.router.get('/',
+        this.beforeFindAll.bind(this),
+        this.findAll.bind(this),
+        this.afterFindAll.bind(this));
+    }
 
-    this.router.get('/:resourceId',
-      this.beforeFind.bind(this),
-      this.find.bind(this),
-      this.afterFind.bind(this));
+    if(options.enabledActions.find) {
+      this.router.get('/:resourceId',
+        this.beforeFind.bind(this),
+        this.find.bind(this),
+        this.afterFind.bind(this));
+    }
 
-    this.router.post('/', 
-      this.beforeCreate.bind(this),
-      this.create.bind(this),
-      this.afterCreate.bind(this));
+    if(options.enabledActions.create) {
+      this.router.post('/', 
+        this.beforeCreate.bind(this),
+        this.create.bind(this),
+        this.afterCreate.bind(this));
+    }
 
-    return this.router;
-	}
+    if(options.enabledActions.update) {
+      this.router.put('/:resourceId',
+        this.beforeUpdate.bind(this),
+        this.update.bind(this),
+        this.afterUpdate.bind(this));
+    }
+
+    if(options.enabledActions.destroy) {
+      this.router.post('/', 
+        this.beforeCreate.bind(this),
+        this.create.bind(this),
+        this.afterCreate.bind(this));
+    }
+    
+    return this;
+  }
   errorHandler(res) {
     return function(error) {
       res.status(500)
@@ -44,4 +78,4 @@ class JSDataRouter {
 }
 
 module.exports = JSDataRouter;
-Object.assign(JSDataRouter.prototype, findAll, find, create);
+Object.assign(JSDataRouter.prototype, findAll, find, create, update);
