5d92b925bc8c91eda8959e1fe5530be451f126e9
David Ramirez 2 <david.x.ramirez@jefferson.edu>
2016-01-01 21:16:54 -0500

    Converted tabs to spaces

--- src/find.js
+++ src/find.js
@@ -1,17 +1,17 @@
 module.exports = {
 
-	beforeFind: function(req, res, next) {
-		next();
-	},
+  beforeFind: function(req, res, next) {
+    next();
+  },
 
-	find: function(req, res, next) {
-		next();
-	},
+  find: function(req, res, next) {
+    next();
+  },
 
-	afterFind: function(req, res, next) {
-		res.status(200)
-	    .send(req.resource)
-	    .end();
-	}
+  afterFind: function(req, res, next) {
+    res.status(200)
+      .send(req.resource)
+      .end();
+  }
 
 };
--- src/param.js
+++ src/param.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next, resourceId) {
-	this.resource.find(resourceId, Object.assign({with: req.with},req.query))
-	  .then(function(resource) {
-	    req.resource = resource;
-	    next();
-	  });
+  this.resource.find(resourceId, Object.assign({with: req.with},req.query))
+    .then(function(resource) {
+      req.resource = resource;
+      next();
+    });
 };
