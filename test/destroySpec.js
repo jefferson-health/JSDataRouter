bd62e008376de478ab67b2e61343309912e938ba
Dave Ramirez <ramirezd42@gmail.com>
2016-01-07 09:29:54 -0500

    fixed some linter errors in the destroySpec and added an afterCreate test

--- test/destroySpec.js
+++ test/destroySpec.js
@@ -8,18 +8,17 @@ describe('destroy', function() {
 
     it('should call next by default', () => {
       let nextSpy = sinon.spy();
-      destroyFns.beforeDestroy({}, {}, nextSpy)
+      destroyFns.beforeDestroy({}, {}, nextSpy);
       nextSpy.should.have.been.calledOnce;
-    })
-  })
+    });
+  });
 
   describe('destroy', function() {
-
     it('should call destroy on this.resource and then call next', function (done) {
-      let resolved = Promise.resolve(true)
+      let resolved = Promise.resolve(true);
       let resourceMock = {
         destroy: sinon.stub().returns(resolved)
-      }
+      };
       let nextSpy = sinon.spy();
       let req = {
         resource: {
@@ -27,7 +26,7 @@ describe('destroy', function() {
             id: 1
           }
         }
-      }
+      };
       let _this = {
         resource: resourceMock,
         errorHandler: sinon.spy()
@@ -36,7 +35,32 @@ describe('destroy', function() {
       resolved.then(function() {
         nextSpy.should.have.been.calledOnce;
         done();
-      })
-    })
-  })
-})
+      });
+    });
+  });
+
+  describe('afterDestroy', function() {
+    it('should send a 200 response by default', function() {
+        let res = {
+          status: function() {
+            return res;
+          },
+          end: function() {
+            return res;
+          },
+          send: function() {
+            return res;
+          }
+        };
+        sinon.spy(res,'status');
+        sinon.spy(res,'send');
+        sinon.spy(res,'end');
+        destroyFns.afterDestroy({}, res);
+        res.status.should.have.been.calledOnce;
+        res.status.should.have.been.calledWith(200);
+        res.send.should.have.been.calledOnce;
+        res.end.should.have.been.calledOnce;
+        res.end.should.have.been.calledAfter(res.send);
+    });
+  });
+});
